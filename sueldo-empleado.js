ce9fbb2ea8364ec575d99e11a3b5a7e4f2751d41
ivana di biase <delfi.17mdq@gmail.com>
2022-03-20 22:40:34 -0300

    ejercicio empleado listo

---
+++ sueldo-empleado.js
@@ -0,0 +1,22 @@
+const readline = require('readline-sync');
+
+const amountByYear = 30;
+let totalAmount = 0;
+
+const hourValue = readline.question('Ingrese el valor de hora trabajada: ');
+const name = readline.question('Ingrese el nombre del empleado: ');
+const yearsWorked = readline.question('Ingrese los años de antiguedad del empleado: ');
+const hoursQty = readline.question('ingrese la cantidad de horas trabajadas: ');
+
+totalAmount += hoursQty * hourValue;
+
+if (yearsWorked > 10 ){
+    totalAmount += yearsWorked * amountByYear;
+}
+
+const message = `
+Empleado: ${name}
+Antiguedad: ${yearsWorked} años
+Total a Cobrar: ${totalAmount}
+`;
+console.log(message);
